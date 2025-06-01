baad0abe9a1ef65c4c779e5a44f59a990da0cea9
rjmlaird <rjmlaird@gmail.com>
2025-06-01 09:19:36 +0100

    🔄 Sync changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
-// https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [tailwind(), compress()],
   buildOptions: {
-    // Set the output directory inside the project
-    outDir: './dist', // This is relative to your project folder (greenorbit-web)
+    outDir: './dist',
   },
 });
